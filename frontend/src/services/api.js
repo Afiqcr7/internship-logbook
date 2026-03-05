4704a9c4a07ca72c2d9cbdf74e3758ab09f29675
Afiqcr7 <ku.afiq.nazhan@gmail.com>
2026-03-05 15:42:50 +0800

    Fix API baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({ 
-  baseURL: 'https://internship-logbook-1.onrender.com/api' // Note the /api
+  baseURL: 'https://internship-logbook-1.onrender.com' // Note the /api
 });
 
 // Automatically attach token to requests
